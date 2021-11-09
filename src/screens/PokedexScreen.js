8b4106198e924649ab93fc41e314a73c8db0c7b3
Eduardo Jami <eduardolalojami@gmail.com>
2021-11-09 18:13:44 -0500

    [add] api call and use of .env with react-native-dotenv

--- src/screens/PokedexScreen.js
+++ src/screens/PokedexScreen.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { getPokemonsApi } from '../api/pokemon';
 
 export default function PokedexScreen() {
+  useEffect(() => {
+    (async ()=>{
+      await loadPokemons();
+    })()
+  }, [])
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi();
+      console.log(response)
+    } catch (error) {
+      console.error(error)
+    }
+  }
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
