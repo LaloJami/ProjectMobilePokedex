b61779557b15820f3583728f95943e3988bd92a4
Eduardo Jami <eduardolalojami@gmail.com>
2021-11-10 10:53:46 -0500

    [add] infinite Scrolling con Flatlist

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import { StyleSheet, Text, FlatList } from 'react-native'
+import { StyleSheet, FlatList, ActivityIndicator } from 'react-native'
 import PokemonCard from './PokemonCard'
 
 export default function PokemonList(props) {
-  const { pokemons } = props
+  const { pokemons, loadPokemons, isNext } = props
 
+  const loadMore = () => {
+    loadPokemons();
+  }
   
   return (
     <FlatList
@@ -12,10 +15,19 @@ export default function PokemonList(props) {
       numColumns={2}
       showsVerticalScrollIndicator={false}
       keyExtractor={(pokemon)=> String(pokemon.id)}
-      renderItem={({item})=>(
-        <PokemonCard pokemon={item} />
-      )}
+      renderItem={({ item }) => <PokemonCard pokemon={item} />}
       contentContainerStyle={styles.flatListContentContainer}
+      onEndReached={isNext && loadMore}
+      onEndReachedThreshold={0.1}
+      ListFooterComponent={
+        isNext && (
+          <ActivityIndicator
+            size="large"
+            style={styles.spinner}
+            color="#AEAEAE"
+          />
+        )
+      }
     />
   )
 }
@@ -23,5 +35,9 @@ export default function PokemonList(props) {
 const styles = StyleSheet.create({
   flatListContentContainer: {
     paddingHorizontal: 5
+  },
+  spiner: {
+    marginTop: 30,
+    marginBottom: 80
   }
 })
