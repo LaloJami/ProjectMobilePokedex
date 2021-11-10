4fcb27487f51acb5a5d054f61763691a4760d79d
Eduardo Jami <eduardolalojami@gmail.com>
2021-11-10 16:52:30 -0500

    [add] maquetacion del pokemon: seccion del tipo

--- src/screens/PokemonScreen.js
+++ src/screens/PokemonScreen.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import { ScrollView } from 'react-native'
 import { getPokemonDetailsApi } from '../api/pokemon'
 import Header from '../components/Pokemon/Header';
+import Type from '../components/Pokemon/Type';
 
 export default function PokemonScreen(props) {
   const {
@@ -20,7 +21,6 @@ export default function PokemonScreen(props) {
       }
     })()
   }, [params])
-  console.log(params.id)
 
   if(!pokemon) return null;
   return (
@@ -31,6 +31,7 @@ export default function PokemonScreen(props) {
         image={pokemon.sprites.other["official-artwork"].front_default}
         type={pokemon.types[0].type.name}
       />
+      <Type types={pokemon.types} />
     </ScrollView>
   )
 }
