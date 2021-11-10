1f93aea7262ccc3222b5a640ffd13e6968b9efba
Eduardo Jami <eduardolalojami@gmail.com>
2021-11-10 11:26:21 -0500

    [add] consulta de API de detalles de pokemon

--- src/screens/PokemonScreen.js
+++ src/screens/PokemonScreen.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { View, Text } from 'react-native'
+import { getPokemonDetailsApi } from '../api/pokemon'
 
-export default function PokemonScreen() {
+export default function PokemonScreen(props) {
+  const {
+    navigation, 
+    route: {params}} = props;
+  
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(()=>{
+    (async ()=>{
+      try {
+        const response = await getPokemonDetailsApi(params.id)
+        setPokemon(response)
+      } catch (error) {
+        navigation.goBack();
+      }
+    })()
+  }, [params])
+  console.log(params.id)
+
+  if(!pokemon) return null;
   return (
     <View>
       <Text>Estamos en un pokemos</Text>
+      <Text>{pokemon.name}</Text>
     </View>
   )
 }
