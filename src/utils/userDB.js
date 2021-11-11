d9864094e3a82a2b3e84b6781953c0c00ce23531
Eduardo Jami <eduardolalojami@gmail.com>
2021-11-10 19:29:32 -0500

    [add] Pantalla de Mi cuenta

---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: "userTest",
+  password: "123456",
+};
+
+export const userDetails = {
+  username: "userTest",
+  firstName: "Eduardo",
+  lastName: "Jami",
+  email: "eduardo-jami@gmail.com"
+};
